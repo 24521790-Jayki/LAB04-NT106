9a4b4fd7984ca0a58f1de2dc4369d0c4de5fe0eb
24521790-Jayki <24521790@gm.uit.edu.vn>
2025-12-06 18:25:39 +0700

    Add project files.

---
+++ LAB04-N5106/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace LAB04_N5106
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnBai1 = new Button();
+            SuspendLayout();
+            // 
+            // btnBai1
+            // 
+            btnBai1.Location = new Point(42, 33);
+            btnBai1.Name = "btnBai1";
+            btnBai1.Size = new Size(94, 29);
+            btnBai1.TabIndex = 0;
+            btnBai1.Text = "Bài 1";
+            btnBai1.UseVisualStyleBackColor = true;
+            btnBai1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnBai1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnBai1;
+    }
+}
